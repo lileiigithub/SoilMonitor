3d4dc11ef3b6b489200b2d0a5fa9ec234aee6a9a
lucklee <lilei_mymail@foxmail.com>
2019-03-14 11:57:56 +0800

    聚类算法修改
    聚类算法修改

--- src/utility/dbscanClusterImg.py
+++ src/utility/dbscanClusterImg.py
@@ -11,36 +11,31 @@ import time
 time1 = time.time()
 
 class Dbscan_segmention(object):
-    def __init__(self,_img_path):
-        self.img_path = _img_path
-        self.img_arr = cv2.imread(_img_path)
-        self.img_shape = self.img_arr.shape
+    def __init__(self,_img_bgr_arr):
+        self.gbr_arr = _img_bgr_arr
+        self.img_shape = self.gbr_arr.shape
         self.img_width = self.img_shape[0]
         self.img_length = self.img_shape[1]
         self.labels = []
         self.n_clusters_ = 0
-        self.labimg = cv2.cvtColor(self.img_arr, cv2.COLOR_BGR2LAB)
+        self.lab_arr = cv2.cvtColor(self.gbr_arr, cv2.COLOR_BGR2LAB)
 
-    def dbscan_claster(self,model = 0,_EPS = 4):
+    def dbscan_cluster(self,_arr,_EPS = 4):
         # input: lab or rgb cluster; the EPS
         # output: the mean of cluster
-        # mod==0:lab , 1:Rgb
-        # print(self.img_arr)
-        print()
-        print(self.img_path,":")
-        # print(self.img_length,self.img_width)
+
         mean = []
         amount_clusting = []
-        if(model == 1): # choose rgb
-            X = self.img_arr.reshape(-1,3)
-        else:  #  # choose lab
-            X = self.labimg.reshape(-1,3)
+        X = _arr.reshape(-1,3)  # 将数据拉平为 3维
         EPS = _EPS
-        MINPTS = self.img_width*self.img_length/10
+        MINPTS = self.img_width*self.img_length/10  #  MinPts半径设为 w*h # /10
+
         db = DBSCAN(eps=EPS, min_samples=MINPTS).fit(X)
         # core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
         # core_samples_mask[db.core_sample_indices_] = True
         self.labels = db.labels_
+
+        print(self.labels)
         self.n_clusters_ = len(set(self.labels)) - (1 if -1 in self.labels else 0)
         # print("labels:",self.labels)
         # print("n_clusters:",self.n_clusters_)
@@ -52,10 +47,11 @@ class Dbscan_segmention(object):
             # print(i,":",mean)
         ratio = len(self.labels[self.labels[:] == -1]) / len(self.labels)
         print("noise ratio: ", ratio)
-        return mean_of_cluster
+        return mean_of_cluster  # 返回了最后一个聚类中心点，后期需要改为返回最大的类的中心点
 
     def dbscan_claster_ab(self):
-        ab = self.labimg[:,:,1:3]
+        # 将ab值聚类
+        ab = self.lab_arr[:,:,1:3]
         print("ab:", ab)
         mean = []
         amount_clusting = []
@@ -78,25 +74,25 @@ class Dbscan_segmention(object):
         ratio = len(self.labels[self.labels[:] == -1]) / len(self.labels)
         print("noise ratio: ", ratio)
 
-
     def save_segmented_imgs(self,_save_path):
         for i in range(self.n_clusters_):
-            img_arr_3d_flatten = self.img_arr.reshape(-1, 3)  # (r,g,b)
-            img_arr_3d_flatten[self.labels != i] = [225]
-            processed_arr_3d = img_arr_3d_flatten.reshape(self.img_arr.shape)
-            path = os.path.join(_save_path,self.img_path.split(".")[0]+"_c"+str(i)+".png")
-            cv2.imwrite(path, processed_arr_3d)
+            gbr_arr_flatten = self.gbr_arr.reshape(-1, 3)  # (r,g,b)
+            gbr_arr_flatten[self.labels != i] = [255]
+            clustered_arr_3d = gbr_arr_flatten.reshape(self.gbr_arr.shape)
+            cv2.imwrite(_save_path, clustered_arr_3d)
 
 
 if __name__ == '__main__':
     import sys
     # img_path = sys.argv[1] #"data/180524_172941.jpg"
-    img_path = "dbscan_test.png"
+    img_path = "data/dbscan_test_1.png"
     # PATH = "1006-340.png"
-    db = Dbscan_segmention(img_path)
+    img_arr = cv2.imread(img_path)
+    db = Dbscan_segmention(img_arr)
     # db.dbscan_claster_lab()
-    db.dbscan_claster(1,5)
-    db.save_segmented_imgs("tmpImg")
+    db.dbscan_cluster(db.lab_arr, 5)
+    save_path = "data/cluster/dbscan_test_c.png"
+    db.save_segmented_imgs(save_path)
     time2 = time.time()
     print('time:', time2 - time1)
 
